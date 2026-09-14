82d619b6a0a2e8191ab6c70e4d4f058dfcb90b80
Matías Muntaner <matiasmuntaner@gmail.com>
2026-09-14 20:56:32 -0300

    users:add get user endpoint

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from fastapi import FastAPI
-from database.database import engine, Base
-import models.user
-from routers import users
-
-# Crea las tablas en la base de datos si no existen
-Base.metadata.create_all(bind=engine)
+from endpoints import users
 
 app = FastAPI(title="FootLel API", version="0.1.0")
 
 app.include_router(users.router)
 
-@app.get("/")
+"""@app.get("/")
 def read_root():
     return {"mensaje": "¡Bienvenido al backend modular de FootLel!"}
+"""
