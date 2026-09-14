dd17853978199293c2b6e8ea3c6dda34cc127f38
Matías Muntaner <matiasmuntaner@gmail.com>
2026-09-14 12:24:42 -0300

    prueba

--- routers/users.py
+++ routers/users.py
@@ -1,7 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from database.database import get_db
+from models.user import User
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.get("/")
-def get_users():
-    return [{"id": 1, "nombre": "Matías"}, {"id": 2, "nombre": "Admin"}]
+def get_users(db: Session = Depends(get_db)):
+    return db.query(User).all()
+
+@router.post("/")
+def create_user(nombre: str, db: Session = Depends(get_db)):
+    new_user = User(nombre=nombre)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
