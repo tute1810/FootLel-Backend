9caaaf8a5901ba2614e82f06e0c001ed4a2607fc
Matías Muntaner <matiasmuntaner@gmail.com>
2026-09-16 20:56:31 -0300

    routers: added authenticaction functions

--- main.py
+++ main.py
@@ -11,3 +11,15 @@ app = FastAPI(title="FootLel_api", version="0.1.0")
 # INCLUDE THE ENDPOINTS TO THE API #
 app.include_router(user_management.router)
 app.include_router(authentication.router)
+
+
+from fastapi.middleware.cors import CORSMiddleware
+
+# Esto le dice al backend "dejá pasar las peticiones que vengan de otros dominios"
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Ojo: En producción es mejor poner tu dominio exacto del frontend en vez de "*"
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
