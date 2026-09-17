98db7a411d536b9a086d7a2a6f0bf6f8c11345a6
gonzalo <gonzalolarranaga40@gmail.com>
2026-09-17 18:09:28 -0300

    database: user management and athentification functions actualization

--- database/authentication.py
+++ database/authentication.py
@@ -7,65 +7,72 @@ current_db_url = deployed_db_url
 
 
 
-def check_existing_user_name(user_name: str):
+
+def is_user_name_used(user_name: str):
     """check if the user name is already in use in a active user returns true to indicate that an user already exists or false"""
-    conn = db.connect(current_db_url)
-    run = conn.cursor()
-       
-        # Comprobar nombre en usuarios activos
-    run.execute("""SELECT 1 FROM users WHERE user_name = (%s) AND user_eliminated = FALSE""",(user_name,))
-    check_user_name = run.fetchone()
-    if check_user_name is not None:
-        conn.rollback()
-        return True
-        
-    conn.commit()
-    return False
+    try:
+        conn = db.connect(current_db_url)
+        run = conn.cursor()
+           
+            # Comprobar nombre en usuarios activos
+        run.execute("""SELECT 1 FROM users WHERE user_name = (%s) AND user_eliminated = FALSE""",(user_name,))
+        check_user_name = run.fetchone()
+        if check_user_name is not None:
+            conn.rollback()
+            return True
+            
+        conn.commit()
+    except:
+        return False
     
-def check_existing_user_email(user_email: str):
+def is_user_email_used(user_email: str):
     
     """Check if the user email is already in use by an active user, returns true to indicate that a user already exists or false"""
-    
-    conn = db.connect(current_db_url)
-    run = conn.cursor()
-    
-    run.execute("""SELECT 1 FROM users WHERE user_email = (%s) AND user_eliminated = FALSE""",(user_email,))
-    check_user_email = run.fetchone()
-    if check_user_email is not None:
-        conn.rollback()
-        return True
-
-    conn.commit()
-    return False
+    try:
+        conn = db.connect(current_db_url)
+        run = conn.cursor()
+        
+        run.execute("""SELECT 1 FROM users WHERE user_email = (%s) AND user_eliminated = FALSE""",(user_email,))
+        check_user_email = run.fetchone()
+        if check_user_email is not None:
+            conn.rollback()
+            return True
+        conn.commit()
+    except: 
+        return False
 
 
     
 def register_new_user(user_name: str, user_email: str, user_password: str):
-    """Sets new user with: user_name: str, user_email: str, user_password: str"""
-    conn = db.connect(current_db_url)
-    run = conn.cursor()
-        # Obtener siguiente ID
-    run.execute(
-            "SELECT COALESCE(MAX(pk_user_id), 0) + 1 FROM users"
-        )
-    user_id = run.fetchone()[0]
+    try:
         
-    run.execute(
-            """
-            INSERT INTO users
-            (pk_user_id, user_name, user_email, user_password, user_eliminated)
-            VALUES (%s, %s, %s, %s, false)
-            """,
-            (user_id, user_name, user_email, user_password)
-        )
-        
-    run.execute(""" INSERT INTO user_configuration (pk_user_id) VALUES (%s)""", (user_id,))
-        
-    run.execute(""" INSERT INTO user_stats (pk_user_id) VALUES (%s)""", (user_id,))
-        
+        """Sets new user with: user_name: str, user_email: str, user_password: str"""
+        conn = db.connect(current_db_url)
+        run = conn.cursor()
+            # Obtener siguiente ID
+        run.execute(
+                "SELECT COALESCE(MAX(pk_user_id), 0) + 1 FROM users"
+            )
+        user_id = run.fetchone()[0]
+            
+        run.execute(
+                """
+                INSERT INTO users
+                (pk_user_id, user_name, user_email, user_password, user_eliminated)
+                VALUES (%s, %s, %s, %s, false)
+                """,
+                (user_id, user_name, user_email, user_password)
+            )
+            
+        run.execute(""" INSERT INTO user_configuration (pk_user_id) VALUES (%s)""", (user_id,))
+            
+        run.execute(""" INSERT INTO user_stats (pk_user_id) VALUES (%s)""", (user_id,))
+            
 
-    conn.commit()
-    
-    return True
-
-
+        conn.commit()
+        
+        
+        return True
+    except:
+        conn.rollback()
+        return False
--- database/user_management.py
+++ database/user_management.py
@@ -6,103 +6,100 @@ current_db_url = deployed_db_url
 
 
 
-def get_users_info():
-    """Get users info in a list """
-    conn = db.connect(current_db_url)
-    run = conn.cursor()
-    
-    run.execute("SELECT * FROM users", )
-    players = run.fetchall()
-    
-    conn.commit()
-    return players
-
-
-
-
-
 def get_user_name(user_id):
     """Get user_name in form of a string using user_id"""
-    conn = db.connect(current_db_url)
-    run = conn.cursor()
-        
-    run.execute("SELECT user_name FROM users WHERE pk_user_id = (%s) AND user_eliminated = false", (user_id,) )
-    user_name = run.fetchone()
-        
-    conn.commit()
+    try:
+        conn = db.connect(current_db_url)
+        run = conn.cursor()
+            
+        run.execute("SELECT user_name FROM users WHERE pk_user_id = (%s) AND user_eliminated = false", (user_id,) )
+        user_name = run.fetchone()
+            
+        conn.commit()
 
 
-    if user_name == None:
-        conn.rollback()
+        if user_name == None:
+            conn.rollback()
+            conn.close()
+            return None
+        
         conn.close()
-        return None
-    
-    conn.close()
-    return user_name[0]   
+    except:        
+        return user_name[0]   
 
 
 
 def get_user_password_with_user_name(user_name: str):
     """Get user password with: user_name: str in form of a string or None if the user doesnt exist or is deactivated"""
-    conn = db.connect(current_db_url)
-    run = conn.cursor()
-    
-    run.execute("SELECT user_password FROM users WHERE user_name = (%s) AND user_eliminated = false", (user_name,))
-    user_password = run.fetchone()
-    
-    conn.commit()
-    if user_password == None:
-        conn.rollback()
-        return user_password
-    else:
+    try:
+        conn = db.connect(current_db_url)
+        run = conn.cursor()
+        
+        run.execute("SELECT user_password FROM users WHERE user_name = (%s) AND user_eliminated = false", (user_name,))
+        user_password = run.fetchone()
+        
+        conn.commit()
+        if user_password == None:
+            conn.rollback()
+            return user_password
+     except:   
         return user_password[0]
 
 def get_user_id(user_email: str):
+    # Comprobar nombre o user_email en usuarios activos
     
     """Get active user id with user_email: str"""
-    
-    conn = db.connect(current_db_url)
-    run = conn.cursor()
-    
-    run.execute(
-    """SELECT pk_user_id FROM users WHERE (user_email = %s) AND user_eliminated = FALSE""", (user_email,))
+    try:
+        conn = db.connect(current_db_url)
+        run = conn.cursor()
         
-    user_id = run.fetchone()
+        run.execute(
+        """SELECT pk_user_id FROM users WHERE (user_email = %s) AND user_eliminated = FALSE""", (user_email,))
+            
+        user_id = run.fetchone()
 
-    if user_id is None:
-        conn.rollback()
-        return None
-    
-    
-    
-    return user_id[0]
+        if user_id is None:
+            conn.rollback()
+            return None
+        
+        
+    except:        
+        return user_id[0]
 
 def eliminate_user(user_id: int):
     """Eliminates user with: user_id: int"""
-    conn = db.connect(current_db_url)
-    run = conn.cursor()
-        
-    run.execute("UPDATE users SET user_eliminated = TRUE WHERE pk_user_id = (%s) ", (user_id,))
-    conn.commit()
-    return True
+    try:
+        conn = db.connect(current_db_url)
+        run = conn.cursor()
+            
+        run.execute("UPDATE users SET user_eliminated = TRUE WHERE pk_user_id = (%s) ", (user_id,))
+        conn.commit()
+        return True
+    except:
+        conn.rollback()
 
 def change_password(user_id: int, user_password: str):
     """Change passowrd from active user with user_id: int and user_password: str """
-    conn = db.connect(current_db_url)
-    run = conn.cursor()
-    
-    run.execute(""" UPDATE users SET user_password = (%s) WHERE pk_user_id = (%s)""", (user_password,user_id ))
-    
-    conn.commit()
-    
-    return True
-
+    try:
+        conn = db.connect(current_db_url)
+        run = conn.cursor()
+        
+        run.execute(""" UPDATE users SET user_password = (%s) WHERE pk_user_id = (%s)""", (user_password,user_id ))
+        
+        conn.commit()
+        
+        return True
+    except:
+        conn.rollback()
 def change_user_data(user_id:int, user_name: str, user_email: str, user_password: str):
     """Change the user user_email, user_name and the user_password with user_name:str, user_email, user_password using user_id)"""
-    conn = db.connect(current_db_url)
-    run = conn.cursor()
-    
-    run.execute(""" UPDATE users SET user_name = %s, user_email = %s, user_password = %s  WHERE pk_user_id = (%s) """, (user_name, user_email, user_password, user_id))
-    conn.commit()
-    conn.close()
-    return True    
+    try:
+        conn = db.connect(current_db_url)
+        run = conn.cursor()
+        
+        run.execute(""" UPDATE users SET user_name = %s, user_email = %s, user_password = %s  WHERE pk_user_id = (%s) """, (user_name, user_email, user_password, user_id))
+        conn.commit()
+        conn.close()
+        return True    
+    except:
+        conn.rollback()   
