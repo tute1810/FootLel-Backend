d5d2fbe3db1edf7e8713c498d41859b1c34eda4f
Martin <larram1324@gmail.com>
2026-09-18 17:41:18 -0300

    router: result messages were reviewed to follow conventions

--- routers/authentication.py
+++ routers/authentication.py
@@ -15,28 +15,28 @@ router = APIRouter()
 @router.post("/auth/user-login")
 def user_login(user_login_credentials: user_login_credentials.UserLoginCredentials):
     if authentication.is_user_name_available(user_login_credentials.user_name) == True:
-        return { "result": "Login Error: user name incorrect" }
+        return { "result": "login error: user name incorrect" }
 
     user_password = user_management.get_user_password_with_user_name(user_login_credentials.user_name)
 
     if user_password is None:
-        return { "result": "Login Error: user name was not found" }
+        return { "result": "login error: user name was not found" }
 
     if user_password != user_login_credentials.user_password:
-        return { "result": "Login Error: user password is incorrect" }
+        return { "result": "login error: user password is incorrect" }
 
     return { "result": "success" }
     
 @router.post("/auth/user-register")
 def user_register(user_register_credentials: user_register_credentials.UserRegisterCredentials):
     if authentication.is_user_name_available(user_register_credentials.user_name) == False:
-        return { "result": "Register Error: user name already in use" }
+        return { "result": "register error: user name already in use" }
 
     if authentication.is_user_email_available(user_register_credentials.user_email) == False:
-        return { "result": "Register Error: user email already in use" }
+        return { "result": "register error: user email already in use" }
 
     if authentication.register_new_user(user_register_credentials.user_name, user_register_credentials.user_email, user_register_credentials.user_password) == False:
-        return { "result": "Register Error: could not register user. Maybe try again?" }
+        return { "result": "register error: could not register user. maybe try again?" }
 
     return { "result": "success" }
     
@@ -45,6 +45,6 @@ def guest_login():
     guest_name: str = "guest_" + str(randint(1000, 9999))
 
     if authentication.is_user_name_available(guest_name) == False:
-        return { "result": "Guest Error: user name already in use. Maybe Try again?" }
+        return { "result": "guest error: user name already in use. maybe Try again?" }
 
     return { "result": "success", "guest_name": guest_name }
--- routers/user_management.py
+++ routers/user_management.py
@@ -15,7 +15,7 @@ def get_user_id(user_email: user_email.UserEmail):
     user_id = user_management.get_user_id(user_email.user_email)
 
     if user_id is None:
-        return { "result": "User Id Error: user with the specified email was not found" }
+        return { "result": "user id error: user with the specified email was not found" }
 
     return { "result": "success", "user_id": int(user_id) }
 
@@ -24,6 +24,6 @@ def get_user_name(user_id: user_id.UserId):
     user_name = user_management.get_user_name(int(user_id.user_id))
 
     if user_name is None:
-        return { "result": "User Name Error: user with the specified id was not found" }
+        return { "result": "user name error: user with the specified id was not found" }
 
     return { "result": "success", "user_name": str(user_name) }
