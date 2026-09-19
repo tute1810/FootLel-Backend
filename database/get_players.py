f460a1dacfb4edde3729a23c0874797f23368a02
gonzalo <gonzalolarranaga40@gmail.com>
2026-09-19 18:02:28 -0300

    database: add get players

---
+++ database/get_players.py
@@ -0,0 +1,125 @@
+import psycopg2 as db
+from psycopg2 import sql
+local_db_url = "postgresql://footlel_db_user:4s67LSdik8NJwqG5N6dCeFd3W46fcZaF@dpg-daks7dlbedkc73cttod0-a.oregon-postgres.render.com/footlel_db"
+deployed_db_url = "postgresql://footlel_db_user:4s67LSdik8NJwqG5N6dCeFd3W46fcZaF@dpg-daks7dlbedkc73cttod0-a/footlel_db"
+current_db_url = local_db_url
+
+def get_league_teams(league: str):
+    """Get league teams"""
+    conn = None
+
+    try:
+        conn = db.connect(current_db_url)
+        run = conn.cursor()
+        
+        
+
+        query = sql.SQL("""
+            SELECT DISTINCT {league}
+            FROM players
+            WHERE {league} IS NOT NULL
+        """).format(
+            league=sql.Identifier(league)
+        )
+
+        run.execute(query)
+
+        teams = run.fetchall()
+        conn.commit()
+
+        if teams == []:
+            conn.rollback()
+            conn.close()
+            return None
+
+        conn.close()
+        return teams 
+    except Exception as e:
+        if conn:
+            conn.rollback()
+        print(e)
+        return None
+    finally:
+        if conn:
+            conn.close()
+            
+            
+def get_random_players(team: str, league: str):
+    """Get league teams"""
+    conn = None
+
+    try:
+        conn = db.connect(current_db_url)
+        run = conn.cursor()
+        
+        columns = {
+            "premier": "premier",
+            "bundes": "bundesliga",
+            "serie_a":"serie_a",
+            "la_liga":"la_liga"
+            }
+        column = columns[league]
+
+        
+
+        run.execute(f"""SELECT player_name, nationality FROM players WHERE {column} = %s ORDER BY RANDOM() LIMIT 3""",  (team,))
+
+        teams = run.fetchall()
+        conn.commit()
+
+        if teams == []:
+            conn.rollback()
+            conn.close()
+            return None
+
+        conn.close()
+        return teams 
+    except Exception as e:
+        if conn:
+            conn.rollback()
+        print(e)
+        return None
+    finally:
+        if conn:
+            conn.close()
+            
+            
+            
+def get_player(team: str, league: str, nationality: str):
+    """Get league teams"""
+    conn = None
+
+    try:
+        conn = db.connect(current_db_url)
+        run = conn.cursor()
+        
+        columns = {
+            "premier": "premier",
+            "bundes": "bundesliga",
+            "serie_a":"serie_a",
+            "la_liga":"la_liga"
+            }
+        column = columns[league]
+
+        
+
+        run.execute(f"""SELECT player_name, nationality FROM players WHERE {column} = %s AND nationality = (%s) """,  (team,nationality))
+
+        teams = run.fetchall()
+        conn.commit()
+
+        if teams == []:
+            conn.rollback()
+            conn.close()
+            return None
+
+        conn.close()
+        return teams 
+    except Exception as e:
+        if conn:
+            conn.rollback()
+        print(e)
+        return None
+    finally:
+        if conn:
+            conn.close()
