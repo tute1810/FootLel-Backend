44273e95bceaad88b538148f604afce1028425a1
Martin <larram1324@gmail.com>
2026-09-20 18:08:54 -0300

    router: added game router

--- routers/game_manager.py
+++ routers/game_manager.py
@@ -20,9 +20,26 @@ def create_table():
     N3    |   |
     """
 
-    leagues_cout: int = 4
+    leagues_count: int = 4
     leagues_name = ["premier", "bundesliga", "serie_a", "la_liga"]
+    nationalities_count: int = 5
+    nationalities_name = ["argentina", "italia", "inglaterra", "españa", "francia"]
 
-    this_league_index: int = randint(0, leagues_cout - 1)
-    # teams_of_random_league: list[str] = 
-    return { "result": str(get_players.get_league_teams(leagues_name[this_league_index])) }
+    teams: list[str] = [3]
+    nationalities: list[str] = [3]
+    players: list[str] = [3]
+    result: str = ""
+    for i in range(0, 3, 1):
+        this_league_index: int = randint(0, leagues_count - 1)
+        random_league: str = leagues_name[this_league_index]
+        teams_of_random_league: list[str] = get_players.get_league_teams(random_league, teams[0], teams[1])
+        random_team_of_random_league: str = teams_of_random_league[randint(0, len(teams_of_random_league) - 1)][0]
+        teams[i] = random_team_of_random_league
+        if len(nationalities) == 0:
+            for j in range(0, 3, 1):
+                this_nationality_index: int = randint(0, nationalities_count - 1)
+                random_nationality: str = nationalities_name[this_nationality_index]
+                nationalities[j] = random_nationality
+        players[i] = get_players.get_player(teams[i], random_league, nationalities[i])
+        result += "\n " + str(teams[i]) + " | " + str(random_league) + " | " + str(nationalities[i])
+    return { "result": result}
