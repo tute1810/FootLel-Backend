2eb54dcfb176267a594c785d7f876ce6f9612469
Martin <larram1324@gmail.com>
2026-09-25 19:39:33 -0300

    router: added game router

--- objects/boolean_state.py
+++ objects/boolean_state.py
@@ -4,4 +4,4 @@ from pydantic import BaseModel
 # CREATE THE OBJECT #
 class BooleanState(BaseModel):
     user_id: int
-    boolean_state: bool
+    boolean: bool
--- routers/user_stats.py
+++ routers/user_stats.py
@@ -11,21 +11,21 @@ router = APIRouter()
 # ENDPOINT FUNCTIONS #
 @router.post("/stat/set-user-dark-mode-state")
 def set_dark_mode_state(boolean_state: boolean_state.BooleanState):
-    if user_stats.set_dark_mode_config_state(boolean_state.user_id, boolean_state.boolean_state) == False:
+    if user_stats.set_dark_mode_config_state(boolean_state.user_id, boolean_state.boolean) == False:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="error de dark mode: no se pudo guardar el valor")
     
     return { "result": "success" }
 
 @router.post("/stat/set-user-animation-state")
 def set_animation_state(boolean_state: boolean_state.BooleanState):
-    if user_stats.set_animations_config_state(boolean_state.user_id, boolean_state.boolean_state) == False:
+    if user_stats.set_animations_config_state(boolean_state.user_id, boolean_state.boolean) == False:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="error de animacion: no se pudo guardar el valor")
     
     return { "result": "success" }
 
 @router.post("/stat/set-user-compatibility-state")
 def set_compatibility_state(boolean_state: boolean_state.BooleanState):
-    if user_stats.set_compatibility_config_status(boolean_state.user_id, boolean_state.boolean_state) == False:
+    if user_stats.set_compatibility_config_status(boolean_state.user_id, boolean_state.boolean) == False:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="error de compatibilidad: no se pudo guardar el valor")
     
     return { "result": "success" }
