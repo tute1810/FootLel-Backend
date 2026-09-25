ddc4c3a7e20ca15861be8ff67147b980ebdf20ec
Martin <larram1324@gmail.com>
2026-09-25 20:27:29 -0300

    router: added game router

--- routers/game.py
+++ routers/game.py
@@ -11,10 +11,10 @@ router = APIRouter()
 # ENDPOINT FUNCTIONS #
 @router.post("/game/start")
 def start_game():
-    column_headers: list[str]; row_headers: list[str]
-    column_headers, row_headers = start_game_board()
+    row_headers: list[str]; column_headers: list[str];
+    row_headers, column_headers = start_game_board()
     
-    return { "result": "success", "column_headers": column_headers, "row_headers": row_headers, "local_player_turn": True }
+    return { "result": "success", "row_headers": row_headers, "column_headers": column_headers, "local_player_turn": True }
 
 @router.post("/game/player-guess")
 def player_guesses(player_guess: player_guess.PlayerGuess):
@@ -24,11 +24,13 @@ def player_guesses(player_guess: player_guess.PlayerGuess):
     if updated_board is None:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="error de tablero: no estas en una partida")
 
-    is_local_players_turn: bool = next_turn()
+    is_local_players_turn: bool = False
+    if game_ended == False:
+        is_local_players_turn = next_turn()
 
     return { "result": "success", "correct_answer": correct_answer, "game_ended": game_ended, "local_player_won": local_player_won, "board": updated_board, "local_player_turn": is_local_players_turn }
 
-@router.post("/game/player-guess")
+@router.post("/game/player2-guess")
 def player2_guesses(player2_guess: player_guess.PlayerGuess):
     updated_board: list[list[int]]; correct_answer: bool; game_ended: bool; local_player_won: bool;
     updated_board, correct_answer, game_ended, local_player_won = player2_guessed(player2_guess.player_guess)
@@ -36,7 +38,9 @@ def player2_guesses(player2_guess: player_guess.PlayerGuess):
     if updated_board is None:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="error de tablero: no estas en una partida")
 
-    is_local_players_turn: bool = next_turn()
+    is_local_players_turn: bool = False
+    if game_ended == False:
+        is_local_players_turn = next_turn()
 
     return { "result": "success", "correct_answer": correct_answer, "game_ended": game_ended, "local_player_won": local_player_won, "board": updated_board, "local_player_turn": is_local_players_turn }
 
@@ -49,6 +53,8 @@ def ai_guesses():
     if updated_board is None:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="error de tablero: no estas en una partida")
 
-    is_local_players_turn: bool = next_turn()
+    is_local_players_turn: bool = False
+    if game_ended == False:
+        is_local_players_turn = next_turn()
 
     return { "result": "success", "game_ended": game_ended, "local_player_won": local_player_won, "board": updated_board, "local_player_turn": is_local_players_turn }
